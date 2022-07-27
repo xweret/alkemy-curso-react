069f5e25de45feccf52ba38ea3e2051abfad5bec
Gabriel Garofalo <gabriel695681@gmail.com>
2022-07-27 18:59:00 -0300

    proteccion de la seccion admin

--- src/components/Detalle.js
+++ src/components/Detalle.js
@@ -1,6 +1,12 @@
+import {Redirect } from "react-router-dom";
 function Detalle (){
+    // let navigate = useNavigate();
+    let token = localStorage.getItem("token");
     return (
+        <>
+        { !token && <Redirect to="/" />}
         <h2>Detalles de la pelicula</h2>
+        </>
     )
 
 }
